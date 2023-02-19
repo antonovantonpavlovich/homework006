bcf9c883c0a3d1dc21296ceec843e6388706813a
Anton <9264622898@mail.ru>
2023-02-19 23:13:44 +0300

    homework006

---
+++ Task001/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine ("Введите числа через пробел");
+string [] strArray = Console.ReadLine().Split(' ');
+
+int [] result = new int[strArray.Length];
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = int.Parse(strArray[i]);
+} 
+Console.WriteLine(string.Join(' ', result));
+
+int GetMax (int[] array)
+{
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i] > 0)
+max++;
+}
+
+return max;
+}
+
+int max = GetMax (result);
+Console.WriteLine(max);
---
+++ Task002/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Напишите программу, которая найдёт 
+точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int GetNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result))
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не корректное число. Повторите ввод");
+}
+}
+
+return result;
+}
+
+double b1 = GetNumber ("Введите координат b1: ");
+double k1 = GetNumber ("Введите координат k1: ");
+double b2 = GetNumber ("Введите координат b2: ");
+double k2 = GetNumber ("Введите координат k2: ");
+
+var x = (b1-b2)/(k2-k1);
+var y = k2 * x + b2;
+   
+
+Console.WriteLine($"({x}; {y})");
+
